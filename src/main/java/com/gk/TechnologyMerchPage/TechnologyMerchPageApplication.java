98df82627b70e10e524f5990d837103e80848ea2
Kaleyfry <59694186+Kaleyfry@users.noreply.github.com>
2020-02-21 17:05:01 -0500

    First commit

---
+++ src/main/java/com/gk/TechnologyMerchPage/TechnologyMerchPageApplication.java
@@ -0,0 +1,13 @@
+package com.gk.TechnologyMerchPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyMerchPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyMerchPageApplication.class, args);
+	}
+
+}
